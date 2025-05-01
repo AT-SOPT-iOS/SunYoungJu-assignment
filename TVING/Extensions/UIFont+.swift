53fed77bd3f2acc0dc4f6bef798e7b7c0bd0b776
SunyoungJu <sts07190@naver.com>
2025-05-02 03:05:31 +0900

    feat: Pretendard 폰트 UIFont 확장 및 전체 적용

--- TVING/Extensions/UIFont+.swift
+++ TVING/Extensions/UIFont+.swift
@@ -8,8 +8,10 @@
 import UIKit
 
 extension UIFont {
-    
-    // MARK: - Pretendard Custom Fonts
+
+    static func pretendardBlack(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Black", size: size)!
+    }
 
     static func pretendardBold(ofSize size: CGFloat) -> UIFont {
         return UIFont(name: "Pretendard-Bold", size: size)!
@@ -19,11 +21,27 @@ extension UIFont {
         return UIFont(name: "Pretendard-ExtraBold", size: size)!
     }
 
+    static func pretendardExtraLight(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-ExtraLight", size: size)!
+    }
+
+    static func pretendardLight(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Light", size: size)!
+    }
+
     static func pretendardMedium(ofSize size: CGFloat) -> UIFont {
         return UIFont(name: "Pretendard-Medium", size: size)!
     }
 
+    static func pretendardRegular(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Regular", size: size)!
+    }
+
     static func pretendardSemiBold(ofSize size: CGFloat) -> UIFont {
         return UIFont(name: "Pretendard-SemiBold", size: size)!
+    }
+
+    static func pretendardThin(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Thin", size: size)!
     }
 }
