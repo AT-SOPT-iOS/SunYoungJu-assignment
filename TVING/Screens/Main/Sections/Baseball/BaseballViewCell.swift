27623401cd3dcda6ca98670b1912159de562d52c
SunyoungJu <sts07190@naver.com>
2025-05-02 21:36:27 +0900

    feat: BaseballViewController & BaseballViewCell - 셀 크기 및 간격 수정

--- TVING/Screens/Main/Sections/Baseball/BaseballViewCell.swift
+++ TVING/Screens/Main/Sections/Baseball/BaseballViewCell.swift
@@ -4,61 +4,60 @@
 //
 //  Created by 선영주 on 5/1/25.
 //
+
 import UIKit
+import SnapKit
+import Then
 
 class BaseballViewCell: UICollectionViewCell {
-    
-    private let backgroundContainer: UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        view.clipsToBounds = true
-        return view
-    }()
-    
-    private let logoImageView: UIImageView = {
-        let iv = UIImageView()
-        iv.contentMode = .scaleAspectFit
-        iv.clipsToBounds = true
-        return iv
-    }()
-    
+
+    // MARK: - UI 컴포넌트
+
+    private let backgroundContainer = UIView().then {
+        $0.clipsToBounds = true
+        $0.backgroundColor = .white
+    }
+
+    private let logoImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.clipsToBounds = true
+    }
+
+    // MARK: - 초기화
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupLayout()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:)는 구현되지 않았습니다.")
     }
-    
+
+    // MARK: - 레이아웃
+
     private func setupLayout() {
         contentView.addSubview(backgroundContainer)
         backgroundContainer.addSubview(logoImageView)
-        
-        backgroundContainer.translatesAutoresizingMaskIntoConstraints = false
-        logoImageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            backgroundContainer.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            backgroundContainer.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            backgroundContainer.widthAnchor.constraint(equalToConstant: 90),
-            backgroundContainer.heightAnchor.constraint(equalToConstant: 45),
-            
-            logoImageView.topAnchor.constraint(equalTo: backgroundContainer.topAnchor, constant: 5),
-            logoImageView.leadingAnchor.constraint(equalTo: backgroundContainer.leadingAnchor, constant: 5),
-            logoImageView.trailingAnchor.constraint(equalTo: backgroundContainer.trailingAnchor, constant: -5),
-            logoImageView.bottomAnchor.constraint(equalTo: backgroundContainer.bottomAnchor, constant: -5)
-        ])
+
+        backgroundContainer.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(80)
+            make.height.equalTo(50)
+        }
+
+        logoImageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.height.equalTo(45)
+        }
     }
-    
+
+    // MARK: - 데이터 설정
+
     func configure(with model: BaseballModel, index: Int) {
         logoImageView.image = model.teamLogo
-        
+
         // index가 짝수면 흰색, 홀수면 검정색 배경
-        if index % 2 == 0 {
-            backgroundContainer.backgroundColor = .white
-        } else {
-            backgroundContainer.backgroundColor = .black
-        }
+        backgroundContainer.backgroundColor = (index % 2 == 0) ? .white : .black
     }
 }
--- TVING/Screens/Main/Sections/Baseball/BaseballViewController.swift
+++ TVING/Screens/Main/Sections/Baseball/BaseballViewController.swift
@@ -4,40 +4,57 @@
 //
 //  Created by 선영주 on 5/1/25.
 //
+
 import UIKit
+import SnapKit
+import Then
 
 class BaseballViewController: UIViewController {
 
+    // MARK: - 데이터
+
     private let baseballItems: [BaseballModel] = BaseballModel.dummy()
 
-    private lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.itemSize = CGSize(width: 90, height: 40)
-        layout.minimumLineSpacing = 7
-        layout.sectionInset = UIEdgeInsets(top: 5, left: 10, bottom: 0, right: 10)
+    // MARK: - UI 컴포넌트
 
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.backgroundColor = .black
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        collectionView.register(BaseballViewCell.self, forCellWithReuseIdentifier: "BaseballViewCell")
-        return collectionView
-    }()
+    private lazy var collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewFlowLayout().then {
+            $0.scrollDirection = .horizontal
+            $0.itemSize = CGSize(width: 80, height: 50)
+            $0.minimumLineSpacing = 7
+            $0.sectionInset = UIEdgeInsets(top: 5, left: 15, bottom: 0, right: 15)
+        }
+    ).then {
+        $0.backgroundColor = .black
+        $0.dataSource = self
+        $0.delegate = self
+        $0.register(BaseballViewCell.self, forCellWithReuseIdentifier: "BaseballViewCell")
+    }
+
+    // MARK: - 생명주기
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .black
+        setupLayout()
+    }
+
+    // MARK: - 레이아웃
+
+    private func setupLayout() {
         view.addSubview(collectionView)
 
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            collectionView.heightAnchor.constraint(equalToConstant: 90)
-        ])
+        collectionView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(80) // 셀보다 살짝 높여서 여유 공간
+            make.bottom.equalToSuperview() // 혹시 부모 뷰의 높이가 다를 때 대비
+        }
     }
 }
+
+// MARK: - UICollectionView 데이터소스 & 델리게이트
 
 extension BaseballViewController: UICollectionViewDataSource, UICollectionViewDelegate {
 
