f5e6750358ee3c0107ba265612d9e6b5a5e11596
SunyoungJu <sts07190@naver.com>
2025-06-03 23:42:44 +0900

    feat: 티빙 메인뷰

---
+++ TVING/tving/tvingApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvingApp.swift
+//  tving
+//
+//  Created by 선영주 on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct tvingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
